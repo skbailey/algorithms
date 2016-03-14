83cf0ce9ab241b61c87eb61cdc12d60be54a0619
Sherard Bailey <sbailey@xogrp.com>
2016-03-13 23:06:30 -0400

    Add algorithms for sorting

---
+++ README.md
@@ -0,0 +1,5 @@
+# Algoriths in JavaScript
+
+Thanks to [NCZOnline](https://www.nczonline.net/blog/tag/algorithms/) for the code
+
+I'm working through algorithmic analysis examples
---
+++ bubble_sort.js
@@ -0,0 +1,21 @@
+function swap(items, firstIndex, secondIndex){
+    var temp = items[firstIndex];
+    items[firstIndex] = items[secondIndex];
+    items[secondIndex] = temp;
+}
+
+function bubbleSort(items){
+
+  var len = items.length,
+      i, j, stop;
+
+  for (i = 0; i < len; i++){
+    for (j = 0, stop = len - i; j < stop; j++){
+      if (items[j] > items[j + 1]){
+        swap(items, j, j + 1);
+      }
+    }
+  }
+
+  return items;
+}
